38eb771b4e341961dba81ccee195fc97c3602548
dixant <dixantsharma095@gmail.com>
2020-04-22 13:11:35 +0530

    facility object data added

---
+++ src/data/facilityData.js
@@ -0,0 +1,59 @@
+const facilityData = [
+    {
+        day: 'Sun',
+        checked: false,
+        fromTime: '00:00',
+        fromAmPm: 'AM',
+        toTime: '00:00',
+        toAmPm: 'AM',
+    },
+    {
+        day: 'Mon',
+        checked: false,
+        fromTime: '00:00',
+        fromAmPm: 'AM',
+        toTime: '00:00',
+        toAmPm: 'AM',
+    },
+    {
+        day: 'Tue',
+        checked: false,
+        fromTime: '00:00',
+        fromAmPm: 'AM',
+        toTime: '00:00',
+        toAmPm: 'AM',
+    },
+    {
+        day: 'Wed',
+        checked: false,
+        fromTime: '00:00',
+        fromAmPm: 'AM',
+        toTime: '00:00',
+        toAmPm: 'AM',
+    },
+    {
+        day: 'Thr',
+        checked: false,
+        fromTime: '00:00',
+        fromAmPm: 'AM',
+        toTime: '00:00',
+        toAmPm: 'AM',
+    },
+    {
+        day: 'Fri',
+        checked: false,
+        fromTime: '00:00',
+        fromAmPm: 'AM',
+        toTime: '00:00',
+        toAmPm: 'AM',
+    },
+    {
+        day: 'Sat',
+        checked: false,
+        fromTime: '00:00',
+        fromAmPm: 'AM',
+        toTime: '00:00',
+        toAmPm: 'AM',
+    }
+]
+export default facilityData;
