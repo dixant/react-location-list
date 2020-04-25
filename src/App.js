fc85a05e304e0f657e9272396900f3f276ff57ba
dixant <dixantsharma095@gmail.com>
2020-04-25 15:23:52 +0530

    App.js refactored

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import './style/App.scss';
 import Location from './components/Location';
-import LocationBlank from './components/LocationBlank';
 
 function App() {
   return (
     <div>
       <Location></Location>
-      
-
     </div>
   );
 }
