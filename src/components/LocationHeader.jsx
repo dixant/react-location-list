a57b44bad7eb1f9bca98af587d0b30540c79df43
dixant <dixantsharma095@gmail.com>
2020-04-25 15:25:20 +0530

    location header refactored

--- src/components/LocationHeader.jsx
+++ src/components/LocationHeader.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import { Navbar, NavbarBrand } from "reactstrap";
 import LocationForm from './LocationForm';
 
-const LocationHeader = ({ locationData, addLocationHandler, addLoactionError, addLoactionErrorMsg }) => {
+const LocationHeader = ({ locationData, addLocationHandler, addLoactionError, addLoactionErrorMsg, closeModal, setCloseModalFalse }) => {
     return (
         <Navbar>
             <NavbarBrand href="/">Location</NavbarBrand>
             <LocationForm buttonLabel="+ Add Location"
                 className="add-location-form"
                 locationData={locationData}
+                closeModal={closeModal}
                 addLocationHandler={addLocationHandler}
                 addLoactionError={addLoactionError}
-                addLoactionErrorMsg={addLoactionErrorMsg}>
+                addLoactionErrorMsg={addLoactionErrorMsg}
+                setCloseModalFalse={setCloseModalFalse}>
             </LocationForm>
         </Navbar>
     )
